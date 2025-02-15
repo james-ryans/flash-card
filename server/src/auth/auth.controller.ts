ac08f8836377c1d378628c0d78a6cc57ff7a468b
James <james@Jamess-MacBook-Air.local>
2025-02-15 12:29:11 +0700

    add user db seeds

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, HttpStatus, Post, Req, UseGuards } from '@nestjs/common';
+import { Controller, Get, HttpCode, HttpStatus, Logger, Post, Req, UseGuards } from '@nestjs/common';
 import { LocalAuthGuard } from 'src/guards/local.guard';
 import { Request } from 'express';
 
@@ -22,6 +22,22 @@ export class AuthController {
         });
         return {
             message: 'Logout successful',
+            statusCode: HttpStatus.OK,
+        };
+    }
+
+    @Post('verify')
+    @HttpCode(HttpStatus.OK)
+    verify(@Req() req: Request): any {
+        Logger.log(req.user);
+        if (req.user === undefined) {
+            return {
+                message: 'User not verified',
+                statusCode: HttpStatus.UNAUTHORIZED,
+            };
+        }
+        return {
+            message: 'User verified',
             statusCode: HttpStatus.OK,
         };
     }
