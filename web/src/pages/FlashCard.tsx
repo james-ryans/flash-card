15be89b694d4905b6124e8becc2c9056c8611302
James <james@192.168.1.47>
2025-02-24 00:14:22 +0700

    refactor page

--- web/src/pages/FlashCard.tsx
+++ web/src/pages/FlashCard.tsx
@@ -1,11 +1,36 @@
-import { Container, Flex, Text } from '@radix-ui/themes';
-import { useSprings, animated, to as interpolate } from '@react-spring/web';
+import { Box, Flex, Kbd, Table, Text } from '@radix-ui/themes';
+import {
+  useSprings,
+  animated,
+  to as interpolate,
+  SpringValue,
+  SpringRef,
+} from '@react-spring/web';
 import { useDrag } from '@use-gesture/react';
 import React from 'react';
 import { useClient } from '../hooks/useClient';
 import { recent, RecentResponse } from '../requests/recent';
 import { AxiosResponse } from 'axios';
 import Loading from './Loading';
+
+type CardSpringValue = {
+  x: SpringValue<number>;
+  y: SpringValue<number>;
+  scale: SpringValue<number>;
+  rotateY: SpringValue<number>;
+};
+
+type CardSpringProps = {
+  x: number;
+  y: number;
+  scale: number;
+  rotateY: number;
+};
+
+type CardProps = {
+  front: string;
+  back: string;
+}
 
 const to = (i: number) => ({
   x: 0,
@@ -18,12 +43,54 @@ const from = (_i: number) => ({ x: 0, rotateY: 0, scale: 1.5, y: -1000 });
 
 const trans = (rotateY: number, scale: number) => `perspective(1500px) rotateY(${rotateY}deg) scale(${scale})`;
 
+const lift = (api: SpringRef<CardSpringProps>, id: number, x: number) => {
+  api.start((i: number) => {
+    if (i !== id) return;
+    return {
+      to: {
+        x,
+        scale: 1.1,
+      },
+      config: { friction: 50, tension: 800 },
+    };
+  });
+};
+
+const flip = (api: SpringRef<CardSpringProps>, id: number, props: CardSpringValue) => {
+  api.start((i: number) => {
+    if (i !== id) return;
+    return {
+      to: {
+        x: 0,
+        rotateY: props.rotateY.get() === 180 ? 0 : 180,
+        scale: 1,
+      },
+      config: { friction: 50, tension: 500 },
+    };
+  });
+};
+
+const swipe = (api: SpringRef<CardSpringProps>, id: number, dir: number) => {
+  api.start((i: number) => {
+    if (i !== id) return;
+    return {
+      to: {
+        x: (200 + window.innerWidth) * dir,
+        scale: 1,
+      },
+      config: { friction: 50, tension: 200 },
+    };
+  });
+};
+
 function FlashCard() {
-  const { isIdle, isLoading, data, update } = useClient<{ front: string; back: string }[]>();
+  const { isIdle, isLoading, data, update } = useClient<CardProps[]>();
+  const cards = data ?? [];
+  const gone = React.useRef(0);
 
   React.useEffect(() => {
     update(
-      recent().then((response: AxiosResponse<RecentResponse>) => {
+      recent().then((response: AxiosResponse<RecentResponse>): CardProps[] => {
         return response.data.data.map((recent) => ({
           front: recent.text,
           back: recent.translation,
@@ -32,44 +99,35 @@ function FlashCard() {
     );
   }, []);
 
-  const cards = data ?? [];
-  let flip = false;
-  const gone = new Set<number>(); // The set flags all the cards that are flicked out
   const [props, api] = useSprings(cards.length, (i) => ({
-    ...to(i),
     from: from(i),
-  })); // Create a bunch of springs using the helpers above
-  // Create a gesture, we're interested in down-state, delta (current-pos - click-pos), direction and velocity
-  const bind = useDrag(({ args: [index], down, movement: [mx], direction: [xDir], velocity }) => {
-    const trigger = velocity[0] > 0.2; // If you flick hard enough it should trigger the card to fly out
-    const dir = xDir < 0 ? -1 : 1; // Direction should either point left or right
-    if (!down) {
-      // If button/finger's up and trigger velocity is reached, we flag the card ready to fly out
-      if (trigger) {
-        gone.add(index);
-        flip = false;
-      } else {
-        flip = !flip;
-      }
+    to: to(i),
+  }));
+
+  const bind = useDrag(({ args: [index], down, movement: [xMove], direction: [xDir], velocity: [xVel] }) => {
+    const flick = xVel > 0.2;
+    const dir = xDir < 0 ? -1 : 1;
+
+    if (down) {
+      lift(api, index, xMove);
+      return;
     }
-    api.start((i) => {
-      if (index !== i) return; // We're only interested in changing spring-data for the current spring
-      const isGone = gone.has(index);
-      const x = isGone ? (200 + window.innerWidth) * dir : down ? mx : 0; // When a card is gone it flys out left or right, otherwise goes back to zero
-      const rotateY = flip ? 180 : 0;
-      const scale = down ? 1.1 : 1; // Active cards lift up a bit
-      return {
-        x,
-        rotateY,
-        scale,
-        delay: undefined,
-        config: { friction: 50, tension: down ? 800 : isGone ? 200 : 500 },
-      };
-    });
-    if (!down && gone.size === cards.length) {
+
+    if (!flick) {
+      flip(api, index, props[index]);
+      return;
+    }
+
+    swipe(api, index, dir);
+    gone.current++;
+
+    if (gone.current === cards.length) {
       setTimeout(() => {
-        gone.clear();
-        api.start((i) => to(i));
+        gone.current = 0;
+        api.start((i) => ({
+          to: to(i),
+          delay: i * 100,
+        }));
       }, 600);
     }
   });
@@ -79,14 +137,10 @@ function FlashCard() {
   }
 
   return (
-    <Container width="100vw" height="calc(100vh - 52px)" className="overflow-x-hidden">
-      <Flex height="100%" justify="center" align="center" className="relative">
+    <Flex flexGrow="1" direction="column" justify="center" className="overflow-hidden">
+      <Flex width="100%" height="200px" justify="center" align="center" className="relative">
         {props.map(({ x, y, rotateY, scale }, i) => (
-          <animated.div
-            key={i}
-            style={{ x, y }}
-            className="absolute flex h-[160px] w-[400px] touch-none items-center justify-center will-change-transform"
-          >
+          <animated.div key={i} style={{ x, y }} className="absolute flex h-40 w-100 touch-none will-change-transform">
             <Card>
               <animated.div {...bind(i)} style={{ transform: interpolate([rotateY, scale], trans) }}>
                 <Text size="9" color="gray" highContrast>
@@ -104,7 +158,44 @@ function FlashCard() {
           </animated.div>
         ))}
       </Flex>
-    </Container>
+      <Box mx="auto" width="fit-content">
+        <Table.Root>
+          <Table.Body>
+            <Table.Row>
+              <Table.Cell>
+                <Text size="1">Flip</Text>
+              </Table.Cell>
+              <Table.Cell justify="end">
+                <Text color="gray" size="1">
+                  Left-click
+                </Text>
+              </Table.Cell>
+              <Table.Cell justify="end">
+                <Kbd>Space</Kbd>
+              </Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell>
+                <Text size="1">Next card</Text>
+              </Table.Cell>
+              <Table.Cell justify="end">
+                <Text color="gray" size="1">
+                  Swipe left/right
+                </Text>
+              </Table.Cell>
+              <Table.Cell justify="end">
+                <Kbd>←</Kbd>
+                <Text color="gray" size="1">
+                  {' '}
+                  or{' '}
+                </Text>
+                <Kbd>→</Kbd>
+              </Table.Cell>
+            </Table.Row>
+          </Table.Body>
+        </Table.Root>
+      </Box>
+    </Flex>
   );
 }
 
