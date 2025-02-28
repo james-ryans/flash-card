78386c909f6f0ba7acfa932c01344bbb9d4371d3
James <james@192.168.1.4>
2025-03-01 01:11:09 +0700

    refactor translation api

--- web/src/requests/translation.ts
+++ web/src/requests/translation.ts
@@ -1,24 +1,10 @@
-import axios, { AxiosError, AxiosResponse } from 'axios';
-import { ErrorResponse } from '../models/common';
 import { TranslationRequest, TranslationResponse } from '../models/translation';
+import { post } from './common';
 
 async function translate(request: TranslationRequest): Promise<string> {
-  return await axios
-    .post(import.meta.env.VITE_SERVER_BASE_URL + '/translate', request, {
-      withCredentials: true,
-    })
-    .then((response: AxiosResponse<TranslationResponse>) => {
-      return response.data.data.text;
-    })
-    .catch((error: AxiosError<ErrorResponse>) => {
-      return Promise.reject(
-        new Error(
-          (Array.isArray(error.response?.data.message)
-            ? error.response?.data.message[0]
-            : error.response?.data.message) || error.message,
-        ),
-      );
-    });
+  return await post('/translate', request).then((response: TranslationResponse) => {
+    return response.data.text;
+  });
 }
 
 export { translate };
